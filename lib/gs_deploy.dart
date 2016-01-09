839e8c5e4fec21a8add08cf050a5e40da640301a
Alexandre Roux Tekartik <alex@tekartik.com>
2016-01-09 16:10:16 +0100

    feat: copy instead of sync for deploy

--- lib/gs_deploy.dart
+++ lib/gs_deploy.dart
@@ -3,6 +3,7 @@ library tekartik_deploy.gs_deploy;
 //import 'dart:io';
 
 import 'package:process_run/cmd_run.dart';
+import 'package:path/path.dart';
 
 final ARG_OUT = 'out';
 
@@ -14,5 +15,27 @@ ProcessCmd gsutilRsyncCmd(String src, String dst) {
   return gsutilCmd(args);
 }
 
+ProcessCmd gsutilCopyCmd(String src, String dst, {bool recursive}) {
+  List<String> gsutilArgs = [];
+
+  // Assume a log of files
+  if (recursive == true) {
+    gsutilArgs.add('-m');
+  }
+
+  gsutilArgs.add('cp');
+
+  if (recursive == true) {
+    gsutilArgs.add('-R');
+
+    src = join(src, '*');
+  }
+
+  gsutilArgs.addAll(['-v', '-z', 'html,css,js,json', '-a', 'public-read', src, dst]);
+
+  return gsutilCmd(gsutilArgs);
+}
+
 // gsutil setwebcfg -m index.html gs://gstest.tekartik.com
-ProcessCmd gsDeployCmd(String src, String dst) => gsutilRsyncCmd(src, dst);
+//ProcessCmd gsDeployCmd(String src, String dst) => gsutilRsyncCmd(src, dst);
+ProcessCmd gsDeployCmd(String src, String dst) => gsutilCopyCmd(src, dst, recursive: true);

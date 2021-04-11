9055ccc604cf6cd4bc25cf9293a9d0fe4ef6fb4d
Alexandre Roux <alex@tekartik.com>
2021-04-11 09:51:26 +0200

    fix: windows test

--- tool/run_ci_override.dart
+++ tool/run_ci_override.dart
@@ -1,8 +1,6 @@
 import 'package:dev_test/package.dart';
-import 'package:process_run/shell.dart';
 
 Future main() async {
-  await packageRunCi('.',
-      options: PackageRunCiOptions(noOverride: true, noTest: true));
-  await Shell().run('dart test -j 1 -p vm,chrome');
+  await packageRunCi('.', options: PackageRunCiOptions(noOverride: true));
+  //await Shell().run('dart test -j 1 -p vm,chrome');
 }

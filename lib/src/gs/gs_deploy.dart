3dcb0db17a96d15cdcd8d9b46468d35bd2881053
alex <alex@tekartik.com>
2024-07-06 10:06:57 +0200

    use mime type from file_download_web

--- lib/src/gs/gs_deploy.dart
+++ lib/src/gs/gs_deploy.dart
@@ -1,24 +1,7 @@
 import 'package:path/path.dart';
 import 'package:process_run/shell.dart';
 
-var _mimeTypeMap = {
-  'css': 'text/css',
-  'dart': 'application/dart',
-  'html': 'text/html',
-  'ico': 'image/x-icon',
-  'jpg': 'image/jpeg',
-  'jpeg': 'image/jpeg',
-  'js': 'application/javascript',
-  'json': 'application/json',
-  'png': 'image/png',
-  'svg': 'image/svg+xml',
-  'txt': 'text/plain',
-  'webp': 'image/webp',
-  'woff': 'application/font-woff',
-  'woff2': 'application/font-woff2',
-  'wasm': 'application/wasm',
-  'pdf': 'application/pdf',
-};
+import 'package:tekaly_file_download_web/mime_type.dart';
 
 /// Copy file to google storage, if dst file name is omitted, it uses
 /// the one from the source.
@@ -30,8 +13,7 @@ Future<void> gsCopyFile(String src, String dst, {bool? verbose}) async {
   if (dst.endsWith('/')) {
     dst = url.join(dst, basename(src));
   }
-  var ext = extension(src);
-  var mimeType = _mimeTypeMap[ext.substring(1)]!;
+  var mimeType = filenameMimeType(src);
   var shell = Shell(verbose: verbose);
   await shell.run('gsutil cp $src $dst');
   await shell.run('gsutil -m setmeta -h "Content-Type:$mimeType" $dst');
